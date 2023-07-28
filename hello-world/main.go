5a6e7d736bd14ed2d1e9c2ea376b480cb9f889c1
Ngang Muma <ngangmuma00@gmail.com>
2023-07-28 12:18:08 +0100

    chapter 2

---
+++ hello-world/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Muma")
+}
+
+/*
+	1) White spaces are characters used to represent a new line
+
+	2) Comments help developers live useful info about the code they write
+	there are 2 types of comment single line // and multiple line comments
+	3) package main
+	4) go doc os Exit
+
+*/
---
+++ types/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("32 x 452 = ", 32*452)
+	fmt.Println("132 x 42 = ", 132*42)
+}
+
+/*
+1) Integers are stored in the computer as fixed sized binary digits
+2) 255
+4) A sring is a sequence of characters and its length is found by len("string")
+5) true
+*/
